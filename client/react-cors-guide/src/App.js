14c0869e023b68d1230c3537910c22167b807e31
LUMI <lmig@itu.dk>
2022-05-17 19:32:02 +0200

    Fetching from API

--- client/react-cors-guide/src/App.js
+++ client/react-cors-guide/src/App.js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import "./App.css";
+import * as api from "./API.js";
 function App() {
 	// const makeAPICall = async () => {
 	//   try {
@@ -17,35 +18,21 @@ function App() {
 	//   makeAPICall();
 	// }, [])
 
-	async function getItems() {
-		fetch(`http://localhost:8080/items`, {
-			mode: "cors",
-			method: "GET",
-			headers: {
-				Accept: "application/json",
-				"Content-Type": "application/json",
-				"Access-Control-Allow-Origin": "*",
-			},
-		})
-			.then((response) => response.json())
-			.then((data) => {
-				document.getElementById("data").innerHTML = JSON.stringify(
-					data,
-					null,
-					2
-				);
-			})
-			.catch((e) => {
-				document.getElementById("data").innerHTML = e.message;
-			});
+	async function handleClick() {
+		const items = await api.getBasket(15);
+		document.getElementById("data").innerHTML = JSON.stringify(items, null, 2);
 	}
-	useEffect(() => {
-		getItems();
-	}, []);
+	async function addItemToBasket() {
+		const items = await api.addItemToBasket(15, 7, 3);
+		document.getElementById("data").innerHTML = JSON.stringify(items, null, 2);
+	}
 
 	return (
 		<div className="App">
 			<div id="data">React Cors Guideee</div>
+
+			<button onClick={handleClick}>Downloading data</button>
+			<button onClick={addItemToBasket}>Add item</button>
 		</div>
 	);
 }
